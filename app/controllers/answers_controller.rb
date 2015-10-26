d5a704ef25a78f3813e6bbc7ef01bc521e2f4a34
rachael berecka & lisa folliard <student@epicodus.com>
2015-10-26 15:47:43 -0700

    Vote on answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,17 @@
 class AnswersController < ApplicationController
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+
+    if params[:upVoteAction] == "upvote"
+      @answer.upvote
+      redirect_to root_path
+    end
+  end
 
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
 
+  def upvote
+    vote=self.vote.to_i
+    new_vote_total = vote += 1
+    self.update(:vote => new_vote_total)
+  end
 end
