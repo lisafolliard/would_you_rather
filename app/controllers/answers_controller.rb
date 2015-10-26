0b61e9805cead3a1dd55f507ae71931262ea281b
rachael berecka & lisa folliard <student@epicodus.com>
2015-10-26 16:03:25 -0700

    Add Answers and Questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -14,4 +14,25 @@ class AnswersController < ApplicationController
     end
   end
 
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new()
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:answer)
+  end
 end
